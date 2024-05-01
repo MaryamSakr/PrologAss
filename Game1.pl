419bf41a17d6d8ee703ab05b0b8fdd74d7d9b68d
MaryamSakr <102592232+MaryamSakr@users.noreply.github.com>
2024-05-01 21:02:27 +0300

    Full Game1

--- Game1.pl
+++ Game1.pl
@@ -1,65 +1,92 @@
-
 head([Head|_], Head).
 
-is_in_board(Board ,StartRow , StartCol):-
+is_in_board(Board, Row, Col) :-
+    length(Board, Rows),
+    Rows > 0,
     head(Board, Head),
-    length(Head , Col),
-    length(Board , Row),
-    StartRow < Row,
-    StartRow >= 0,
-    StartCol < Col,
-    StartCol >=0.
+    length(Head, Cols),
+    Row >= 0,
+    Row < Rows,
+    Col >= 0,
+    Col < Cols.
 
-get_color(Col , Row, Board , Color):-
-    nth0(Row , Board , RowList),
-    nth0(Col , RowList , Color).
+get_color(Row, Col, Board, Color) :-
+    nth0(Row, Board, RowList),
+    nth0(Col, RowList, Color).
 
-is_same_color(Col , NewCol , Row , NewRow , Board ):-
-    get_color(Col , Row , Board ,Color),
-    get_color(NewCol , NewRow , Board ,Color).
+is_same_color(Col1, Row1, Col2, Row2, Board) :-
+    get_color(Row1, Col1, Board, Color),
+    get_color(Row2, Col2, Board, Color).
 
-move_right(Board , Row , Col , CurrentRow , CurrentCol , NewCol):-
-    NewCol is CurrentCol+1,
-    is_in_board(Board , CurrentRow , NewCol),
-    is_same_color(CurrentCol , NewCol , CurrentRow , CurrentRow , Board).
-    
+move_right(Board, Row, Col, NewCol) :-
+    NewCol is Col + 1,
+    is_in_board(Board, Row, NewCol),
+    is_same_color(Col, Row, NewCol, Row, Board).
 
-move_down(Board , Row , Col , CurrentRow , CurrentCol , NewRow):-
-    NewRow is CurrentRow+1,
-    is_in_board(Board , NewRow , CurrentCol),
-    is_same_color(CurrentCol , CurrentCol , CurrentRow , NewRow , Board).
-    
+move_down(Board, Row, Col, NewRow) :-
+    NewRow is Row + 1,
+    is_in_board(Board, NewRow, Col),
+    is_same_color(Col, Row, Col, NewRow, Board).
 
-move_up(Board , Row , Col , CurrentRow , CurrentCol , NewRow):-
-    NewRow is CurrentRow-1,
-    is_in_board(Board , NewRow , CurrentCol),
-    is_same_color(CurrentCol , NewCol , CurrentRow , NewRow , Board).
-    
+move_up(Board, Row, Col, NewRow) :-
+    NewRow is Row - 1,
+    is_in_board(Board, NewRow, Col),
+    is_same_color(Col, Row, Col, NewRow, Board).
 
-move_left(Board , Row , Col , CurrentRow , CurrentCol , NewCol):-
-    NewCol is CurrentCol-1,
-    is_in_board(Board , CurrentRow , NewCol),
-    is_same_color(CurrentCol , NewCol , CurrentRow , CurrentRow , Board).
+move_left(Board, Row, Col, NewCol) :-
+    NewCol is Col - 1,
+    is_in_board(Board, Row, NewCol),
+    is_same_color(Col, Row, NewCol, Row, Board).
 
+is_cycle(RootRow, RootCol, Visited, CurrentRow, CurrentCol) :-
+    length(Visited, Length),
+    Length >= 4,
+    CurrentCol =:= RootCol,
+    CurrentRow =:= RootRow + 1,
+    member((CurrentRow, CurrentCol, _), Visited).  % Include color in the visited list
 
-    
+find_cycle(Board, RootRow, RootCol, Visited, Row, Col, CurrentRow, CurrentCol, Cycle) :-
+    move_right(Board, CurrentRow, CurrentCol, RightCol),
+    \+ member((CurrentRow, RightCol, _), Visited),  % Include color in the visited list
+    get_color(CurrentRow, RightCol, Board, Color),  % Get the color at the current position
+    find_cycle(Board, RootRow, RootCol, [(CurrentRow, RightCol, Color) | Visited], Row, Col, CurrentRow, RightCol, Cycle).
 
-find_cycle(Board , RootRow , RootCol ,Visited , Row , Col , CurrentRow , CurrentCol , Cycle):-
-    move_right(Board , Row , Col , CurrentRow , CurrentCol , RightCol),
-    \+(member([(CurrentRow , RightCol)] , Visited)),
-    find_cycle(Board , CurrentRow , CurrentCol , [[(CurrentRow , RightCol)]|Visited] , Row , Col , CurrentRow , RightCol , Cycle);
-     move_down(Board , Row , Col , CurrentRow , CurrentCol , DownRow),
-    \+(member([(DownRow , RightCol)] , Visited)),
-    find_cycle(Board , CurrentRow , CurrentCol , [[(DownRow , RightCol)]|Visited] , Row , Col , DownRow , RightCol , Cycle);
-     move_left(Board , Row , Col , CurrentRow , CurrentCol , LeftCol),
-    \+(member([(DownRow , LeftCol)] , Visited)), 
-    find_cycle(Board ,CurrentRow , CurrentCol , [[(DownRow , LeftCol)]|Visited] , Row , Col , DownRow , LeftCol , Cycle);
-    move_up(Board , Row , Col , CurrentRow , CurrentCol , UpRow),
-	\+(member([(UpRow , LeftCol)] , Visited)).
-    
-example_board(Board ,Cycle):-
+find_cycle(Board, RootRow, RootCol, Visited, Row, Col, CurrentRow, CurrentCol, Cycle) :-
+    move_down(Board, CurrentRow, CurrentCol, DownRow),
+    \+ member((DownRow, CurrentCol, _), Visited),  % Include color in the visited list
+    get_color(DownRow, CurrentCol, Board, Color),  % Get the color at the current position
+    find_cycle(Board, RootRow, RootCol, [(DownRow, CurrentCol, Color) | Visited], Row, Col, DownRow, CurrentCol, Cycle).
+
+find_cycle(Board, RootRow, RootCol, Visited, Row, Col, CurrentRow, CurrentCol, Cycle) :-
+    move_left(Board, CurrentRow, CurrentCol, LeftCol),
+    \+ member((CurrentRow, LeftCol, _), Visited),  % Include color in the visited list
+    get_color(CurrentRow, LeftCol, Board, Color),  % Get the color at the current position
+    find_cycle(Board, RootRow, RootCol, [(CurrentRow, LeftCol, Color) | Visited], Row, Col, CurrentRow, LeftCol, Cycle).
+
+find_cycle(_, RootRow, RootCol, Visited, _, _, CurrentRow, CurrentCol, Cycle) :-
+    is_cycle(RootRow, RootCol, Visited, CurrentRow, CurrentCol),
+    reverse(Visited, Cycle).
+
+find_cycle(Board, Cycle) :-
+    length(Board, Rows),
+    Rows > 0,
     head(Board, Head),
-    length(Head , Col),
-    length(Board , Row),
-    find_cycle(Board , 0 , 0 , [(0,0)] ,Row , Col , 0 , 0 , Cycle).
-    
+    length(Head, Cols),
+    find_cycle(Board, 0, 0, [(0, 0, _)], Rows, Cols, 0, 0, Cycle).  % Include a placeholder for color
+
+example_board(Board, Cycle) :-
+    length(Board, Rows),
+    Rows > 0,
+    head(Board, Head),
+    length(Head, Cols),
+    (
+        between(0, Rows, Row),
+        between(0, Cols, Col),
+        find_cycle(Board, Row, Col, [(Row, Col, _)], Rows, Cols, Row,Col, Cycle),
+        print_cycle(Cycle, Board),  
+        !
+    ).
+
+print_cycle([], _).
+print_cycle([(Row, Col, Color)|_], Board) :-
+    get_color(Row, Col, Board, Color).
